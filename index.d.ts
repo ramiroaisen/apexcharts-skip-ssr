18807ed17c1518bb762bb2d7d4fd01399ee4c980
Ramiro <ramiroaisen@gmail.com>
2023-05-30 16:04:18 -0300

    improve types

--- index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "apexcharts";
-export = import("apexcharts");
---
+++ types.d.ts
@@ -0,0 +1,3 @@
+import ApexCharts from "apexcharts";
+export type { ApexOptions } from "apexcharts";
+export default ApexCharts;
